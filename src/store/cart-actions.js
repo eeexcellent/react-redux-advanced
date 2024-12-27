702ca466ed3f86c530707da7eb13363373513647
excellent <flarpzz08@gmail.com>
2024-12-27 15:19:51 +0100

    add cart data fetching

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,4 +1,41 @@
+import { cartActions } from "./cart-slice";
 import { uiActions } from "./ui-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://test-91035-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Fetching cart data failed!");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const sendCartData = (cart) => {
   return async (dispatch) => {
@@ -15,7 +52,10 @@ export const sendCartData = (cart) => {
         "https://test-91035-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
